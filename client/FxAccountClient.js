124b76ee8997283e3518c6d7b041466c306fc3fb
Ashwin02 <ashwin.s.patil10@gmail.com>
2016-03-30 16:43:21 -0700

    fix(docs): Fix jsdoc for _passwordChangeFinish

--- client/FxAccountClient.js
+++ client/FxAccountClient.js
@@ -871,7 +871,7 @@ define([
    * @param {String} email
    * @param {String} newPassword
    * @param {Object} oldCreds This object should consists of `oldUnwrapBKey`, `keyFetchToken` and `passwordChangeToken`.
-   * @param {Boolean} [options.keys] If `true`, calls the API with `?keys=true` to get the keyFetchToken
+   * @param {Object} keys This object should contain the unbundled keys
    * @return {Promise} A promise that will be fulfilled with JSON of `xhr.responseText`
    */
   FxAccountClient.prototype._passwordChangeFinish = function(email, newPassword, oldCreds, keys) {
