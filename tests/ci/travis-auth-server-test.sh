826f76b92d016aea1d60120dcd1f7e52baace3b3
vladikoff <vlad.filippov@gmail.com>
2017-04-17 12:09:59 -0400

    feat(emails): add api changes and testing changes

--- tests/ci/travis-auth-server-test.sh
+++ tests/ci/travis-auth-server-test.sh
@@ -2,8 +2,12 @@
 
 # Install and start the auth server
 git clone https://github.com/mozilla/fxa-auth-server.git
-cd fxa-auth-server && npm i
-SIGNIN_CONFIRMATION_ENABLED=true SIGNIN_CONFIRMATION_FORCE_EMAIL_REGEX="^confirm.*@restmail\\.net$"  SIGNIN_UNBLOCK_ALLOWED_EMAILS="^block.*@restmail\\.net$" SIGNIN_UNBLOCK_FORCED_EMAILS="^block.*@restmail\\.net$" npm start &
+
+cd fxa-auth-server
+# TODO REMOVE CUSTOM BRANCH
+git checkout add-more-emails
+npm i
+SECONDARY_EMAIL_ENABLED=true SIGNIN_CONFIRMATION_ENABLED=true SIGNIN_CONFIRMATION_FORCE_EMAIL_REGEX="^confirm.*@restmail\\.net$"  SIGNIN_UNBLOCK_ALLOWED_EMAILS="^block.*@restmail\\.net$" SIGNIN_UNBLOCK_FORCED_EMAILS="^block.*@restmail\\.net$" npm start &
 cd ..
 sleep 5
 
