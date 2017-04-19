be38f99bcb58ada6c442a9108973d847a5ec18de
Vijay Budhram <vbudhram@mozilla.com>
2017-04-18 22:21:59 -0400

    feat(emails): Remove custom branch reference

--- tests/ci/travis-auth-server-test.sh
+++ tests/ci/travis-auth-server-test.sh
@@ -2,11 +2,7 @@
 
 # Install and start the auth server
 git clone https://github.com/mozilla/fxa-auth-server.git
-
-cd fxa-auth-server
-# TODO REMOVE CUSTOM BRANCH
-git checkout add-more-emails
-npm i
+cd fxa-auth-server && npm i
 SECONDARY_EMAIL_ENABLED=true SIGNIN_CONFIRMATION_ENABLED=true SIGNIN_CONFIRMATION_FORCE_EMAIL_REGEX="^confirm.*@restmail\\.net$"  SIGNIN_UNBLOCK_ALLOWED_EMAILS="^block.*@restmail\\.net$" SIGNIN_UNBLOCK_FORCED_EMAILS="^block.*@restmail\\.net$" npm start &
 cd ..
 sleep 5
